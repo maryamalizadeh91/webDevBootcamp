5180566565620f004b028333a0d40b59dcf7b8c0
maryamalizadeh91 <maryamalizadeh91@gmail.com>
2019-01-22 10:55:19 +0330

    YelpCamp v4 folder

---
+++ YelpCamp v4/app.js
@@ -0,0 +1,58 @@
+var express         = require("express"), 
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    localStrategy   = require("passport-local"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    seedDB          = require("./seeds");
+
+//requiring routes
+var commentRoutes       = require("./routes/comments"),
+    campgroundRoutes    = require("./routes/campgrounds"),
+    indexRoutes         = require("./routes/index");
+
+//======================//
+//   APP Configuration  //
+//======================//
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+//seed the Database
+//seedDB();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+//======================//
+//PASSPORT Configuration//
+//======================//
+
+app.use(require("express-session")({
+    secret: "YelpCamp is Awesome!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/campgrounds", campgroundRoutes);
+
+//listening to port
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("The YelpCamp server has started!");
+});
